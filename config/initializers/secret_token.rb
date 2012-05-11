82f519a7320e92296a1a467472ac4e82fbd12002
Alexey Nureev <navygator@myopera.com>
2012-05-11 16:41:36 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorktimeTracker::Application.config.secret_token = '6a2ec4c9f8f6c2d812e727f866b0877665eaed3cea7cd54a2ea15aa979f022d3004d588aab0baf7b938b4ede04800cebacbc517227568937a48463406a51692a'
